be9fb2eb3f796f6e8fb728a6a768584d06e29558
KimsPractice <tpghksdldia@naver.com>
2019-03-03 01:45:12 +0900

    상속소스수정

--- Practice/inheritance/inheritance2.php
+++ Practice/inheritance/inheritance2.php
@@ -6,7 +6,7 @@ $file->rewind(); //fread로인한 인덱스초기화
 
 class testFileObject extends SplFileObject {
     function get_contents(){
-      $content = var_dump($this->fread($this->getSize()));
+      $content = $this->fread($this->getSize());
       $this->rewind();
       return $content;
     }
